e9c3b064d93a43f3e9c0c91d3c4a8f1c90087481
arshwetha <leelak125@gmail.com>
2018-10-05 13:23:41 -0400

    Refactoring

--- RiskGameAPP/src/col/cs/risk/controller/StartGameController.java
+++ RiskGameAPP/src/col/cs/risk/controller/StartGameController.java
@@ -1,124 +1,43 @@
 package col.cs.risk.controller;
 
-import java.awt.Color;
-import java.awt.IllegalComponentStateException;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-import javax.swing.BorderFactory;
-import javax.swing.GroupLayout;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import col.cs.risk.view.MapConstructionView;
-import col.cs.risk.helper.Utility;
+
 import col.cs.risk.view.HomePageViewLoader;
+import col.cs.risk.view.MapConstructionView;
 
 /**
  * 
- * @author Team Starts the game
+ * @author Team
+ * Starts the game
  *
  */
-public class StartGameController extends java.awt.Frame {
-
-	/** Serial version id */
-	private static final long serialVersionUID = 1137729697072494580L;
-
+public class StartGameController{
+	
+	/** Game home Page View */
+	private HomePageViewLoader homePageViewLoader;
+	
 	/** Map construction/modification Page View */
 	private MapConstructionView mapConstructionView;
-
-	/** Initial game panel */
-	private JPanel startJPanel;
-
-	/** start game button */
-	private JButton startGameButton;
-
-	/** map construct button */
-	private JButton constructMapButton;
-
-	/** load existing map button */
-	private JButton loadExistingMapButton;
-
-	/** exit button */
-	private JButton exitButton;
-
-	/** game image */
-	private JLabel gameImage;
-
+	
 	/**
 	 * Starts the game page(Loads the home page) with multiple options
 	 */
 	public StartGameController() {
-		initComponents();
-		HomePageViewLoader.loadHomePage(this);
-
+		new HomePageViewLoader(this).setVisible(true);
 	}
 
-	private void initComponents() {
-		startJPanel = new JPanel();
-		startGameButton = new JButton();
-		constructMapButton = new JButton();
-		loadExistingMapButton = new JButton();
-		exitButton = new JButton();
-		gameImage = new JLabel();
+	/**
+	 * @return the homePageViewLoader
+	 */
+	public HomePageViewLoader getHomePageViewLoader() {
+		return homePageViewLoader;
+	}
 
-		startJPanel.setBorder(BorderFactory.createLineBorder(new Color(0, 0, 0)));
-		setBackground(new Color(1, 1, 1));
-		addWindowListener(new WindowAdapter() {
-			public void windowClosing(WindowEvent event) {
-				exitForm(event);
-			}
-		});
-		startJPanel.setName("startJPanel");
-
-		startGameButton.setName("startGameButton");
-		startGameButton.setText("Start Default Game");
-		startGameButton.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent event) {
-				startGameButtonActionPerformed(event);
-			}
-		});
-
-		constructMapButton.setName("constructMapButton");
-		constructMapButton.setText("Construct Map");
-		constructMapButton.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent event) {
-				constructMapButtonActionPerformed(event);
-			}
-		});
-
-		loadExistingMapButton.setName("loadExistingMapButton");
-		loadExistingMapButton.setText("Load Existing Map");
-		loadExistingMapButton.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent event) {
-				loadExistingMapButtonActionPerformed(event);
-			}
-		});
-
-		exitButton.setName("exitButton");
-		exitButton.setText("Exit");
-		exitButton.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent event) {
-				exitButtonActionPerformed(event);
-			}
-		});
-
-		gameImage.setIcon(new ImageIcon(Utility.getImagePath("game_logo.jpg")));
-		gameImage.setName("gameImage");
-
-		GroupLayout groupLayout = new GroupLayout(startJPanel);
-		startJPanel.setLayout(groupLayout);
-
+	/**
+	 * @param homePageViewLoader the homePageViewLoader to set
+	 */
+	public void setHomePageViewLoader(HomePageViewLoader homePageViewLoader) {
+		this.homePageViewLoader = homePageViewLoader;
 	}
 
 	/**
@@ -136,8 +55,7 @@ public class StartGameController extends java.awt.Frame {
 	 * @param event
 	 */
 	public void constructMapButtonActionPerformed(ActionEvent event) {
-		setVisible(false);
-		System.out.println(" Construct Map button pressed ");
+		homePageViewLoader.setVisible(false);
 		new MapConstructionView(this).setVisible(true);
 	}
 
@@ -152,137 +70,15 @@ public class StartGameController extends java.awt.Frame {
 
 	/**
 	 * Action performed on pressing Exit button on home page
-	 * 
 	 * @param event
 	 */
 	public void exitButtonActionPerformed(ActionEvent event) {
 		System.out.println(" Exit button pressed ");
 		System.exit(0);
 	}
-
-	/**
-	 * Start/ entry of the game application
-	 * 
-	 * @param args
-	 */
-	public static void main(String args[]) {
-		java.awt.EventQueue.invokeLater(new Runnable() {
-
-			public void run() {
-				new StartGameController().setVisible(true);
-			}
-		});
-	}
-
-	/**
-	 * Exit the Application.
-	 *
-	 * @param event the event
-	 */
-	private void exitForm(java.awt.event.WindowEvent event) {
-		System.exit(0);
-	}
-
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public void setBackground(Color bgColor) {
-		synchronized (getTreeLock()) {
-			if ((bgColor != null) && (bgColor.getAlpha() < 255) && !isUndecorated()) {
-				throw new IllegalComponentStateException("The frame is decorated");
-			}
-			super.setBackground(bgColor);
-		}
-	}
-
-	/**
-	 * @return the startJPanel
-	 */
-	public JPanel getStartJPanel() {
-		return startJPanel;
-	}
-
-	/**
-	 * @param startJPanel the startJPanel to set
-	 */
-	public void setStartJPanel(JPanel startJPanel) {
-		this.startJPanel = startJPanel;
-	}
-
-	/**
-	 * @return the startGameButton
-	 */
-	public JButton getStartGameButton() {
-		return startGameButton;
-	}
-
-	/**
-	 * @param startGameButton the startGameButton to set
-	 */
-	public void setStartGameButton(JButton startGameButton) {
-		this.startGameButton = startGameButton;
-	}
-
-	/**
-	 * @return the constructMapButton
-	 */
-	public JButton getConstructMapButton() {
-		return constructMapButton;
-	}
-
-	/**
-	 * @param constructMapButton the constructMapButton to set
-	 */
-	public void setConstructMapButton(JButton constructMapButton) {
-		this.constructMapButton = constructMapButton;
-	}
-
-	/**
-	 * @return the loadExistingMapButton
-	 */
-	public JButton getLoadExistingMapButton() {
-		return loadExistingMapButton;
-	}
-
-	/**
-	 * @param loadExistingMapButton the loadExistingMapButton to set
-	 */
-	public void setLoadExistingMapButton(JButton loadExistingMapButton) {
-		this.loadExistingMapButton = loadExistingMapButton;
-	}
-
-	/**
-	 * @return the exitButton
-	 */
-	public JButton getExitButton() {
-		return exitButton;
-	}
-
-	/**
-	 * @param exitButton the exitButton to set
-	 */
-	public void setExitButton(JButton exitButton) {
-		this.exitButton = exitButton;
-	}
-
-	/**
-	 * @return the gameImage
-	 */
-	public JLabel getGameImage() {
-		return gameImage;
-	}
-
-	/**
-	 * @param gameImage the gameImage to set
-	 */
-	public void setGameImage(JLabel gameImage) {
-		this.gameImage = gameImage;
-	}
-
+	
 	/**
 	 * Action performed on pressing New Map construction Button
-	 * 
 	 * @param event
 	 */
 	public void newMapButtonActionPerformed(ActionEvent event) {
@@ -291,7 +87,6 @@ public class StartGameController extends java.awt.Frame {
 
 	/**
 	 * Action performed on pressing Modify Existing Map Button
-	 * 
 	 * @param event
 	 */
 	public void modifyExistingMapButtonActionPerformed(ActionEvent event) {
@@ -310,5 +105,18 @@ public class StartGameController extends java.awt.Frame {
 	 */
 	public void setMapConstructionView(MapConstructionView mapConstructionView) {
 		this.mapConstructionView = mapConstructionView;
+	}
+
+	/**
+	 * Start/ entry of the game application
+	 * @param args
+	 */
+	public static void main(String args[]) {
+		java.awt.EventQueue.invokeLater(new Runnable() {
+
+			public void run() {
+				new StartGameController();
+			}
+		});
 	}
 }
--- RiskGameAPP/src/col/cs/risk/view/HomePageViewLoader.java
+++ RiskGameAPP/src/col/cs/risk/view/HomePageViewLoader.java
@@ -1,59 +1,204 @@
 package col.cs.risk.view;
 
 import java.awt.BorderLayout;
-import javax.swing.GroupLayout;
+import java.awt.Color;
+import java.awt.Frame;
+import java.awt.IllegalComponentStateException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
+import javax.swing.BorderFactory;
+import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.LayoutStyle.ComponentPlacement;
+
 import col.cs.risk.controller.StartGameController;
+import col.cs.risk.helper.Utility;
 
 /**
  * 
- * @author Team Loads the home page
+ * @author Team
+ * Loads the home page
  *
  */
-public class HomePageViewLoader {
+public class HomePageViewLoader extends Frame{
+
+	/**
+	 * Serial version id
+	 */
+	private static final long serialVersionUID = 5937936098745280534L;
+
+	/** Initial game panel */
+	private JPanel startJPanel;
+
+	/** start game button */
+	private JButton startGameButton;
+
+	/** map construct button */
+	private JButton constructMapButton;
+
+	/** load existing map button */
+	private JButton loadExistingMapButton;
+
+	/** exit button */
+	private JButton exitButton;
+
+	/** game image */
+	private JLabel gameImage;
+	
+	/**
+	 * Game controller
+	 */
+	private StartGameController startController;
+	
+	public HomePageViewLoader(StartGameController gameController) {
+		this.startController = gameController;
+		gameController.setHomePageViewLoader(this);
+		initComponents();
+	}
+	
+	/**
+	 * Initialize the components
+	 */
+	public void initComponents() {
+		startJPanel = new JPanel();
+		startGameButton = new JButton();
+		constructMapButton = new JButton();
+		loadExistingMapButton = new JButton();
+		exitButton = new JButton();
+		gameImage = new JLabel();
+
+		startJPanel.setBorder(BorderFactory.createLineBorder(new Color(0, 0, 0)));
+		setBackground(new Color(1, 1, 1));
+		addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent event) {
+				exitForm(event);
+			}
+		});
+		startJPanel.setName("startJPanel");
+
+		startGameButton.setName("startGameButton");
+		startGameButton.setText("Start Default Game");
+		startGameButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent event) {
+				startController.startGameButtonActionPerformed(event);				
+			}
+		});
+
+		constructMapButton.setName("constructMapButton");
+		constructMapButton.setText("Construct Map");
+		constructMapButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent event) {
+				startController.constructMapButtonActionPerformed(event);				
+			}
+		});
+
+		loadExistingMapButton.setName("loadExistingMapButton");
+		loadExistingMapButton.setText("Load Existing Map");
+		loadExistingMapButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent event) {
+				startController.loadExistingMapButtonActionPerformed(event);				
+			}
+		});
+
+		exitButton.setName("exitButton");
+		exitButton.setText("Exit");
+		exitButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent event) {
+				startController.exitButtonActionPerformed(event);				
+			}
+		});
+
+		gameImage.setIcon(new ImageIcon(Utility.getImagePath("game_logo.jpg")));
+		gameImage.setName("gameImage");
+		
+		loadHomePage();
+
+	}
 
 	/**
 	 * 
-	 * @param startController Loads the Game home page
+	 * @param startController 
+	 * Loads the Game home page
 	 */
-	public static void loadHomePage(StartGameController startController) {
+	private void loadHomePage() {
+		
+		GroupLayout groupLayout = new GroupLayout(startJPanel);
+		startJPanel.setLayout(groupLayout);
 
-		GroupLayout groupLayout = new GroupLayout(startController.getStartJPanel());
-		startController.getStartJPanel().setLayout(groupLayout);
-
-		groupLayout.setHorizontalGroup(groupLayout.createParallelGroup(Alignment.TRAILING).addGroup(groupLayout
-				.createSequentialGroup().addContainerGap()
-				.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addGroup(groupLayout.createSequentialGroup().addComponent(startController.getGameImage())
-								.addContainerGap(10, Short.MAX_VALUE))
-						.addGroup(groupLayout.createSequentialGroup().addGap(100).addGroup(groupLayout
-								.createParallelGroup(Alignment.CENTER)
-								.addComponent(startController.getStartGameButton(), GroupLayout.DEFAULT_SIZE, 100, 500)
-								.addGap(50)
-								.addComponent(startController.getConstructMapButton(), GroupLayout.DEFAULT_SIZE, 100,
-										500)
-								.addGap(50)
-								.addComponent(startController.getLoadExistingMapButton(), GroupLayout.DEFAULT_SIZE, 100,
-										500)
-								.addGap(50)
-								.addComponent(startController.getExitButton(), GroupLayout.DEFAULT_SIZE, 100, 500)
-								.addGap(50)).addContainerGap()))));
-		groupLayout.setVerticalGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(groupLayout.createSequentialGroup().addContainerGap()
-						.addComponent(startController.getGameImage())
+		groupLayout.setHorizontalGroup(
+				groupLayout.createParallelGroup(Alignment.TRAILING)
+				.addGroup(groupLayout.createSequentialGroup()
+						.addContainerGap()
+						.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addGroup(groupLayout.createSequentialGroup()
+										.addComponent(gameImage)
+										.addContainerGap(10, Short.MAX_VALUE))
+								.addGroup(groupLayout.createSequentialGroup()
+										.addGap(100)
+										.addGroup(groupLayout.createParallelGroup(Alignment.CENTER)
+												.addComponent(startGameButton, GroupLayout.DEFAULT_SIZE, 100, 500)
+												.addGap(50)
+												.addComponent(constructMapButton,GroupLayout.DEFAULT_SIZE, 100, 500)
+												.addGap(50)
+												.addComponent(loadExistingMapButton,GroupLayout.DEFAULT_SIZE, 100, 500)
+												.addGap(50)
+												.addComponent(exitButton, GroupLayout.DEFAULT_SIZE, 100, 500)
+												.addGap(50)).addContainerGap())))
+				);
+		groupLayout.setVerticalGroup(
+				groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+						.addContainerGap()
+						.addComponent(gameImage)
 						.addPreferredGap(ComponentPlacement.RELATED, 40, Short.MAX_VALUE)
-						.addComponent(startController.getStartGameButton())
+						.addComponent(startGameButton)
 						.addPreferredGap(ComponentPlacement.RELATED, 8, Short.MAX_VALUE)
-						.addComponent(startController.getConstructMapButton())
+						.addComponent(constructMapButton)
 						.addPreferredGap(ComponentPlacement.RELATED, 8, Short.MAX_VALUE)
-						.addComponent(startController.getLoadExistingMapButton())
+						.addComponent(loadExistingMapButton)
 						.addPreferredGap(ComponentPlacement.RELATED, 8, Short.MAX_VALUE)
-						.addComponent(startController.getExitButton()).addContainerGap()));
-
-		startController.add(startController.getStartJPanel(), BorderLayout.CENTER);
-		startController.pack();
+						.addComponent(exitButton)
+						.addContainerGap())
+				);
+		
+		add(startJPanel, BorderLayout.CENTER);
+		pack();
 	}
-
+	
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void setBackground(Color bgColor) {
+		synchronized (getTreeLock()) {
+			if ((bgColor != null) && (bgColor.getAlpha() < 255) && !isUndecorated()) {
+				throw new IllegalComponentStateException("The frame is decorated");
+			}
+			super.setBackground(bgColor);
+		}
+	}
+	
+	/**
+	 * Exit the Application.
+	 *
+	 * @param event the event
+	 */
+	private void exitForm(java.awt.event.WindowEvent event) {
+		System.exit(0);
+	}
 }
