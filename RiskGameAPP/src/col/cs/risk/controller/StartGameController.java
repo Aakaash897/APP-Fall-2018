36a06674f43c2b97146ba09a6fffda24ceab2e71
saikrishnaalla <43051161+saikrishnaalla@users.noreply.github.com>
2018-10-15 11:14:36 -0400

    Add files via upload
    Updated StartGameController File

--- RiskGameAPP/src/col/cs/risk/controller/StartGameController.java
+++ RiskGameAPP/src/col/cs/risk/controller/StartGameController.java
@@ -1,7 +1,16 @@
 package col.cs.risk.controller;
 
 import java.awt.event.ActionEvent;
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
+import col.cs.risk.helper.Utility;
+import col.cs.risk.model.GameModel;
 import col.cs.risk.view.ConstructNewMapView;
 import col.cs.risk.view.HomePageViewLoader;
 import col.cs.risk.view.LoadExistingMapView;
@@ -20,9 +29,11 @@ public class StartGameController{
 	/** Load Existing Map View */
 	private LoadExistingMapView loadingExistingMapView;
 	
-	
 	/** Map construction/modification Page View */
 	private MapConstructionView mapConstructionView;
+	
+	/** Construct New map Page View */
+	private ConstructNewMapView constructNewMapView;
 	
 	/**
 	 * Starts the game page(Loads the home page) with multiple options
@@ -30,6 +41,50 @@ public class StartGameController{
 	public StartGameController() {
 		new HomePageViewLoader(this).setVisible(true);
 		new LoadExistingMapView(this);
+	}
+	
+	/**
+	 * Validates empty text
+	 * @param text
+	 * @return
+	 */
+	public boolean isEmptyDetails(String text) {
+		if(text.isEmpty() ) {
+			return true;
+		}
+		else {
+			String str = text.replaceAll("\\s+","");
+			if(str.isEmpty()) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * Validates Single Line of text
+	 * @param text
+	 * @returns true if single line text else false
+	 */
+	public boolean isSingleLineText(String text) {
+		InputStream is = new ByteArrayInputStream(text.getBytes());
+		BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+		int lineNo = 0;
+		try {
+			while(reader.readLine()!=null) {
+				lineNo++;
+				if(lineNo == 2) {
+					break;
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		if(lineNo > 1) {
+			return false;
+		} else {
+			return true;
+		}
 	}
 
 	/**
@@ -68,6 +123,7 @@ public class StartGameController{
 	 * @param event
 	 */
 	public void startGameButtonActionPerformed(ActionEvent event) {
+		homePageViewLoader.setVisible(false);
 		setPlayers();
 		System.out.println(" Start Default Game button pressed ");
 	}
@@ -76,7 +132,6 @@ public class StartGameController{
 	 * Player settings
 	 */
 	private void setPlayers() {
-		homePageViewLoader.setVisible(false);
 		new PlayerSettingsController();
 	}
 
@@ -143,6 +198,20 @@ public class StartGameController{
 	}
 
 	/**
+	 * @return the constructNewMapView
+	 */
+	public ConstructNewMapView getConstructNewMapView() {
+		return constructNewMapView;
+	}
+
+	/**
+	 * @param constructNewMapView the constructNewMapView to set
+	 */
+	public void setConstructNewMapView(ConstructNewMapView constructNewMapView) {
+		this.constructNewMapView = constructNewMapView;
+	}
+
+	/**
 	 * Start/Entry of the game application
 	 * @param args
 	 */
@@ -153,5 +222,52 @@ public class StartGameController{
 				new StartGameController();
 			}
 		});
+	}
+	
+	/**
+	 * Action performed on create button pressed on new map construction page
+	 * @param line Map section
+	 * @param line1 Continent section
+	 * @param line2 Country section
+	 */
+	public void actionPerformedOnMapCreateButton(String line, String line1, String line2) {
+		StringBuilder mapString = new StringBuilder();
+		mapString.append("[Map]\n");
+		mapString.append(line+"\n\n");
+		mapString.append("[Continents]\n");
+		mapString.append(line1+"\n\n");
+		mapString.append("[Territories]\n");
+		mapString.append(line2+"\n\n");
+		
+		GameModel gameModel = new GameModel();
+		if(isEmptyDetails(line1)) {
+			constructNewMapView.setErrorStatus("Atleast add one continent ");
+		} else if(isEmptyDetails(line2)) {
+			constructNewMapView.setErrorStatus("Add countries in Territory section ");
+		} else if(!gameModel.checkContinentsAreValid(mapString.toString())) {
+			constructNewMapView.setErrorStatus("Add continents which are present in the continent section ");
+		} else if(isEmptyDetails(line1)) {
+			constructNewMapView.setErrorStatus("Add country details in country section");
+		} else if(isSingleLineText(line2)) {
+			constructNewMapView.setErrorStatus("Can't create connected map with a single country");
+		} else if(!gameModel.isAllTerritoriesConnected(mapString.toString())) {
+			constructNewMapView.setErrorStatus("Atleast add one adjacent country ");
+		} else {
+			try
+			{
+				FileWriter fw = new FileWriter(new File(Utility.getMapPath("currMap.map")));
+				fw.write(mapString.toString());
+				fw.close();
+				constructNewMapView.setVisible(false);
+				GameModel.isBaseMapModified = true;
+				GameModel.modifiedMapString = mapString;
+				GameModel.fileName = "currMap.map";
+				setPlayers();
+			}
+			catch(Exception e)
+			{
+				e.printStackTrace();
+			}
+		}
 	}
 }
