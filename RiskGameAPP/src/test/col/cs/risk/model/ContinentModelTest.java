53b04ceaf2da20ba9196ae781dc744330f5328b9
balpreet7 <balpreetsingh7@gmail.com>
2018-11-07 12:37:20 -0500

    Added Test case in Continent Model

--- RiskGameAPP/src/test/col/cs/risk/model/ContinentModelTest.java
+++ RiskGameAPP/src/test/col/cs/risk/model/ContinentModelTest.java
@@ -1,8 +1,15 @@
 package test.col.cs.risk.model;
 
+import static org.junit.Assert.assertFalse;
+
+import java.util.Vector;
+
 import org.junit.Before;
+import org.junit.Test;
 
 import col.cs.risk.model.ContinentModel;
+import col.cs.risk.model.PlayerModel;
+import col.cs.risk.model.TerritoryModel;
 
 public class ContinentModelTest {
 
@@ -11,5 +18,23 @@ public class ContinentModelTest {
 	@Before
 	public void before() {
 		continentModel = new ContinentModel(1, "cName", 1);
+	}
+
+	@Test
+	public void testIsContinentOccupied() {
+		TerritoryModel model1 = new TerritoryModel(201, "tname1", 10, 20, new ContinentModel(301, "cname1", 2));
+		model1.setArmies(5);
+		TerritoryModel model2 = new TerritoryModel(1, "tname2", 30, 40, new ContinentModel(1, "cname2", 3));
+		model2.setArmies(5);
+		PlayerModel playerModel1 = new PlayerModel(2, "pName");
+		model1.setPlayerModel(playerModel1);
+		PlayerModel playerModel2 = new PlayerModel(1, "pName");
+		model2.setPlayerModel(playerModel2);
+		Vector<TerritoryModel> vector = new Vector<>();
+		vector.add(model1);
+		vector.add(model2);
+		continentModel = new ContinentModel(1, "cName", 1, vector);
+		assertFalse(continentModel.isContinentOccupiedBy(playerModel1));
+		assertFalse(continentModel.isContinentOccupiedBy(playerModel2));
 	}
 }
