4bb09d801ba8f3863a03c711446d4fc25528e72a
balpreet7 <balpreetsingh7@gmail.com>
2018-11-07 13:00:38 -0500

    Added Java Doc for Territory Model Test case

--- RiskGameAPP/src/test/col/cs/risk/model/TerritoryModelTest.java
+++ RiskGameAPP/src/test/col/cs/risk/model/TerritoryModelTest.java
@@ -10,15 +10,31 @@ import col.cs.risk.model.ContinentModel;
 import col.cs.risk.model.PlayerModel;
 import col.cs.risk.model.TerritoryModel;
 
+/**
+ * This test class checks if the territory is occupied or not.
+ * 
+ * @author Team25
+ *
+ */
 public class TerritoryModelTest {
+
+	/**
+	 * Territory model instance
+	 */
 	private TerritoryModel territoryModel;
 
+	/**
+	 * Initialization before every test case
+	 */
 	@Before
 	public void before() {
 		territoryModel = new TerritoryModel(201, "tname1", 10, 20, new ContinentModel(301, "cname1", 2));
 
 	}
 
+	/**
+	 * Test case to check whether territory is occupied or not
+	 */
 	@Test
 	public void isOccupied() {
 		assertFalse(territoryModel.isOccupied());
