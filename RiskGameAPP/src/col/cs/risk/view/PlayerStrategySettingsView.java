b62bb9bc9c7c2eb4c8781e158afbfd6f3feeea81
saikrishnaalla <43051161+saikrishnaalla@users.noreply.github.com>
2018-11-25 19:16:37 -0500

    Updated Strategy view of players

--- RiskGameAPP/src/col/cs/risk/view/PlayerStrategySettingsView.java
+++ RiskGameAPP/src/col/cs/risk/view/PlayerStrategySettingsView.java
@@ -20,7 +20,7 @@ public class PlayerStrategySettingsView extends JFrame implements ActionListener
 	        JFrame PlayerStrategySettingsViewFrame;
 	        
 	        /** Label to display Players title */
-		    JLabel currentplayerLabel; 
+		    JLabel playerTitle,currentplayerLabel; 
 		    
 		    /** CheckBoxes for each Strategies */
 		    JCheckBox hcb,acb,bcb,rcb,ccb; 
@@ -30,6 +30,11 @@ public class PlayerStrategySettingsView extends JFrame implements ActionListener
 		    
 		    /** Title Player */
 		    String titlePlayer;
+		    
+		    /**
+		     * Combo box for Players Strategies
+		     */
+		    JComboBox StrategyComboBox1;
 		    
 		    /** No of Players Playing Game */
 		    int noOfPlayers;
@@ -49,56 +54,63 @@ public class PlayerStrategySettingsView extends JFrame implements ActionListener
 			 * @param noOfPlayers No of Players Playing Game 
 			 * @param TitlePlayer Title set as Players
 			 */
-		    public PlayerStrategySettingsView(int noOfPlayers, String TitlePlayer){ 
+		    PlayerStrategySettingsView(int noOfPlayers, String TitlePlayer){ 
 	            PlayerStrategySettingsViewFrame = new JFrame();
-		    	this.titlePlayer = titlePlayer;
+		    	this.titlePlayer = TitlePlayer;
 		    	this.noOfPlayers = noOfPlayers;
 		    	
-		        currentplayerLabel=new JLabel(titlePlayer); 
+		    	playerTitle = new JLabel(titlePlayer); 
 		        PlayerStrategySettingsViewFrame.setTitle("PlayerStartegySettingsPanel");
-		        currentplayerLabel.setBounds(50,50,300,20); 
-		        currentplayerLabel.setForeground(new Color(254, 254, 254));
-				currentplayerLabel.setHorizontalAlignment(SwingConstants.CENTER);
-				currentplayerLabel.setVerticalAlignment(SwingConstants.CENTER);
-				currentplayerLabel.setFont(new Font(Font.MONOSPACED, Font.BOLD, 20));
-				JComboBox StrategyComboBox = new JComboBox();
+		        playerTitle.setBounds(150,50,300,20); 
+		        playerTitle.setForeground(new Color(254, 254, 254));
+		        playerTitle.setHorizontalAlignment(SwingConstants.CENTER);
+		        playerTitle.setVerticalAlignment(SwingConstants.CENTER);
+		        playerTitle.setFont(new Font(Font.MONOSPACED, Font.BOLD, 20));
+		        PlayerStrategySettingsViewFrame.add(playerTitle);
+		    	
+		        int y = 0;
 				for(int i = 1;i<= noOfPlayers;i++)
 				{
-					StrategyComboBox.addItem("player"+i);
+					
+					currentplayerLabel =new JLabel("player"+i); 
+			        PlayerStrategySettingsViewFrame.setTitle("PlayerStartegySettingsPanel");
+			        currentplayerLabel.setBounds(50,150+y,300,20); 
+			        currentplayerLabel.setForeground(new Color(254, 254, 254));
+					currentplayerLabel.setHorizontalAlignment(SwingConstants.CENTER);
+					currentplayerLabel.setVerticalAlignment(SwingConstants.CENTER);
+					currentplayerLabel.setFont(new Font(Font.MONOSPACED, Font.BOLD, 20));
+					PlayerStrategySettingsViewFrame.add(currentplayerLabel);
+					y = y+100;
 				}
-				StrategyComboBox.setVisible(true);
-				StrategyComboBox.setBounds(50,100,300,20);
-				StrategyComboBox.addActionListener(new java.awt.event.ActionListener() {
+	
+				int y1 = 0;
+				for(int i = 1;i<= noOfPlayers;i++) {
+				StrategyComboBox1 = new JComboBox();
+			    StrategyComboBox1.addItem("Human");
+			    StrategyComboBox1.addItem("aggressive");
+			    StrategyComboBox1.addItem("benevolent");
+			    StrategyComboBox1.addItem("random");
+			    StrategyComboBox1.addItem("cheater");
+				StrategyComboBox1.setVisible(true);
+				StrategyComboBox1.setBounds(300,150+y1,200,20);
+				y1 = y1+100;
+				PlayerStrategySettingsViewFrame.getContentPane().add(StrategyComboBox1);
+				StrategyComboBox1.addActionListener(new java.awt.event.ActionListener() {
 						public void actionPerformed(java.awt.event.ActionEvent evt) {
 							
-							HashKey = (String) StrategyComboBox.getSelectedItem();
+							HashKey = (String) StrategyComboBox1.getSelectedItem();
     
 						}
 
 					});
-		        hcb=new JCheckBox("Human");  
-		        hcb.setBounds(100,150,150,20);  
-		        acb=new JCheckBox("aggressive");  
-		        acb.setBounds(100,200,150,20);  
-		        bcb=new JCheckBox("benevolent");  
-		        bcb.setBounds(100,250,150,20); 
-		        rcb=new JCheckBox("random");  
-		        rcb.setBounds(100,300,150,20);
-		        ccb=new JCheckBox("cheater");  
-		        ccb.setBounds(100,350,150,20);
+				}
+				
 		        SaveButton=new JButton("Save");  
-		        SaveButton.setBounds(130,450,80,30);  
+		        SaveButton.setBounds(280,200+y,80,30);  
 		        SaveButton.addActionListener(this); 
 		        PlayerStrategySettingsViewFrame.getContentPane().setBackground(new java.awt.Color(1, 1, 1));
-		        PlayerStrategySettingsViewFrame.add(currentplayerLabel);
-		        PlayerStrategySettingsViewFrame.add(hcb);
-		        PlayerStrategySettingsViewFrame.add(acb);
-		        PlayerStrategySettingsViewFrame.add(bcb);
-		        PlayerStrategySettingsViewFrame.add(rcb);
-		        PlayerStrategySettingsViewFrame.add(ccb);
 		        PlayerStrategySettingsViewFrame.add(SaveButton); 
-		        PlayerStrategySettingsViewFrame.getContentPane().add(StrategyComboBox);
-		        PlayerStrategySettingsViewFrame.setSize(600,600);  
+		        PlayerStrategySettingsViewFrame.setSize(700,1000);  
 		        PlayerStrategySettingsViewFrame.setLayout(null);  
 		        PlayerStrategySettingsViewFrame.setVisible(true); 
  
